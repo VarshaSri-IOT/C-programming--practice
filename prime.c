938ed0761cebf74238ee706fba36fe4f605cf8d7
VarshaSri-IOT <varshasrikankata.05@gmail.com>
2026-06-18 13:20:13 +0530

    added prime program

---
+++ prime.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i, flag =1;
+    printf("enter a number: ");
+    scanf("%d",&n);
+
+    if(n<=1){//n=0,n=1
+        flag =0;
+    }
+    for(i=2;i<n;i++){
+        if(n%i==0){
+            flag=0;
+            break;
+        }
+    }
+    if(flag)
+        printf("prime number ");
+    else
+        printf("not a prime number" );
+
+    return 0;
+}
